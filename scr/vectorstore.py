2eec90356084ee470ff5fe5bd7016e8e43c1a551
Hossein Moein <moeinhochen@gmail.com>
2026-07-06 21:47:44 +0330

    Update vectorstore.py

--- scr/vectorstore.py
+++ scr/vectorstore.py
@@ -1,22 +1,25 @@
 import faiss
 import numpy as np
+
+
 class VectorStore:
     def __init__(self, dimension):
         self.index = faiss.IndexFlatL2(dimension)
-        self.texts=[]
+        self.texts = []
 
-    def add(self, embedding,texts):
-        embeddings = np.asarray(embedding).astype('float32')
+    def add(self, embeddings, texts):
+        embeddings = np.asarray(embeddings).astype("float32")
         self.index.add(embeddings)
         self.texts.extend(texts)
 
     def search(self, query_embedding, k=3):
-        query_embedding = np.array([query_embedding]).astype("float32") #[] for 2D
+        query_embedding = np.array([query_embedding]).astype("float32")
+
         distances, indices = self.index.search(query_embedding, k)
 
-        results = [self.texts[i] for i in indices[0]]
+        results = []
+        for i in indices[0]:
+            if i != -1:
+                results.append(self.texts[i])
+
         return results
-
-
-
-
