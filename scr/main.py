76869f16ce05841bfa3fdc80b6f7c4ae969bff14
Hossein Moein <moeinhochen@gmail.com>
2026-07-06 21:40:19 +0330

    Create main.py

---
+++ scr/main.py
@@ -0,0 +1,5 @@
+from ingest import extract_text_from_pdf
+from chunking import clean_text, split_into_sentences, create_chunks
+from embeddings import get_embeddings
+from vectorstore import VectorStore
+from sentence_transformers import SentenceTransformer
