88d10e419f3db3e70dce37322c288789e82735be
Hossein Moein <moeinhochen@gmail.com>
2026-07-06 21:35:28 +0330

    Create vectorstore.py

---
+++ scr/vectorstore.py
@@ -0,0 +1,20 @@
+class VectorStore:
+    def __init__(self, dimension):
+        self.index = faiss.IndexFlatL2(dimension)
+        self.texts=[]
+
+    def add(self, embedding,texts):
+        embeddings = np.asarray(embedding).astype('float32')
+        self.index.add(embeddings)
+        self.texts.extend(texts)
+
+    def search(self, query_embedding, k=3):
+        query_embedding = np.array([query_embedding]).astype("float32") #[] for 2D
+        distances, indices = self.index.search(query_embedding, k)
+
+        results = [self.texts[i] for i in indices[0]]
+        return results
+
+
+
+
