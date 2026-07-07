ad0495ad85561726b7e291f2bc6a0a0e5fd2f6cb
Hossein Moein <moeinhochen@gmail.com>
2026-07-07 23:13:53 +0330

    Update pipeline.py

--- scr/pipeline.py
+++ scr/pipeline.py
@@ -1,12 +1,64 @@
-from pipeline import RAGPipeline
+from ingest import extract_text_from_pdf
+from chunking import clean_text,  split_into_sentences, create_chunks
+from embeddings import get_embeddings
+from vectorstore import VectorStore
+
+#%%
+class RAGPipeline:
+    def __init__(self,pdf):
+        self.pdf = pdf
+
+        self.chunk = []
+        self.embeddings = None
+        self.vectorstore = None
 
 
-bot = RAGPipeline("Introbiology.pdf")
+    def build(self):
+        text = extract_text_from_pdf(self.pdf)
+        text = clean_text(text)
+        sentences = split_into_sentences(text)
+
+        self.chunk = create_chunks(sentences)
+
+        self.embeddings = get_embeddings(self.chunk)
+
+        self.vectorstore = VectorStore(len(self.embeddings[0]))
+
+        self.vectorstore.add(self.embeddings,self.chunk)
+
+
+    def search(self,question,k=2):
+
+        query_embedding = get_embeddings([question])[0]
+        results = self.vectorstore.search(
+        query_embedding,
+        k
+    )
+        return results
+
+
+
+
+
+
+
+bot = RAGPipeline("introbiology.pdf")
 
 bot.build()
 
-answer = bot.ask(
-    "What is mitochondria?"
+print("Knowledge base created!")
+
+
+
+
+
+
+
+results = bot.search(
+    "What is the function of mitochondria?"
 )
 
-print(answer)
+
+for r in results:
+    print(r)
+
