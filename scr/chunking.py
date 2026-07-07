199b2233e261b2c1f6e1f66514096e77ab63979e
Hossein Moein <moeinhochen@gmail.com>
2026-07-07 22:23:11 +0330

    Update chunking.py

--- scr/chunking.py
+++ scr/chunking.py
@@ -1,40 +1,34 @@
 import re
 
 def clean_text(text):
-    # 1. remove newlines
     text = text.replace("\n", " ")
-
-    # 2. remove numbering like 1. 2.1 10.
-    text = re.sub(r"\b\d+(\.\d+)?\s*\:", "", text)
-
-    # 3. remove bullet points
     text = text.replace("•", "")
-
-    # 4. replace arrows
     text = text.replace("→", " to ")
 
-    # 5. normalize spaces
-    text = " ".join(text.split())
+    # normalize spaces
+    text = re.sub(r"\s+", " ", text)
 
-    return text
+    return text.strip()
 
 
-def split_into_paragraphs(text):
-    sentences = text.split('. ')
-    return [s.strip() for s in sentences if len(s)>20]
+def split_into_sentences(text):
+    sentences = re.split(r'(?<=[.!?])\s+', text)
 
-#each chunk contains 5 sentences
-#each new chunk repeats 2 sentences from previous one
-def create_chunks(sentences,chunk_size=5,overlap=2):
-    chunks =[]
-    i=0
+    return [
+        s.strip()
+        for s in sentences
+        if len(s.split()) >= 4
+    ]
+
+
+def create_chunks(sentences, chunk_size=5, overlap=2):
+    chunks = []
+
+    i = 0
+
     while i < len(sentences):
-        chunk = sentences[i:i+chunk_size]
-        print(chunk)
-        chunks.append(". ".join(chunk))
-        i+= (chunk_size - overlap)
+        chunk = sentences[i:i + chunk_size]
+        chunks.append(" ".join(chunk))
+        i += chunk_size - overlap
 
     return chunks
-
-
-
