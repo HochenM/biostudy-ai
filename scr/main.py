e02068b54e5afc6c00d09c9b4f9bb8af71f386bd
Hossein Moein <moeinhochen@gmail.com>
2026-07-08 18:50:05 +0330

    Update main.py

--- scr/main.py
+++ scr/main.py
@@ -3,3 +3,4 @@ from chunking import clean_text, split_into_sentences, create_chunks
 from embeddings import get_embeddings
 from vectorstore import VectorStore
 from sentence_transformers import SentenceTransformer
+from llm import generate_answer
